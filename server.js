7ab6f2158b66b84420dcf7801d54f693b31f3d66
keemeg <dmdrnsl@gmail.com>
2022-05-31 17:42:14 +0900

    ㄴㄴ

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const http = require("http");
-
-const app = express();
-app.use(express.static(__dirname + "/client"));
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/client/html/index.html");
-});
-const server = http.createServer(app).listen(process.env.PORT || 3000);
